f808c5fdc9513e47ae1882b11d0399e392f2107a
Salif Mehmed <salifm@protonmail.com>
2020-09-28 20:21:26 +0300

    update

--- home/salifm/.config/fish/functions/fish_prompt.fish
+++ home/salifm/.config/fish/functions/fish_prompt.fish
@@ -1,8 +1,33 @@
-function fish_prompt --description 'Write out the prompt'
-    set -l last_status $status
+function fish_prompt
+	set -l last_status $status
+    if not set -q VIRTUAL_ENV_DISABLE_PROMPT
+        set -g VIRTUAL_ENV_DISABLE_PROMPT true
+    end
+    set_color $fish_color_user
+    printf '%s' $USER
     set_color normal
+    printf ' at '
+
+    set_color magenta
+    echo -n (prompt_hostname)
+    set_color normal
+    printf ' in '
+
     set_color $fish_color_cwd
-    echo "> ["$last_status"] "(pwd)
-    echo -n '➤ '
+    printf '%s' (prompt_pwd)
+    set_color normal
+
+    # Line 2
+    echo
+    if test $VIRTUAL_ENV
+        printf "(%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
+    end
+    
+    if not test $last_status -eq 0
+        set_color $fish_color_error
+    end
+
+    printf '➤ '
+    
     set_color normal
 end
--- home/salifm/.profile
+++ home/salifm/.profile
@@ -1,6 +1,4 @@
-
 export QT_QPA_PLATFORMTHEME=qt5ct
-export GPG_TTY=/usr/bin/tty
 export EDITOR=/usr/bin/micro
 export GOPATH=$HOME/go
 export GOROOT=/usr/lib/go
