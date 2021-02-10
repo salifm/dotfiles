1c7715bfbc5cc5257358d1e8c765cfbc8836b644
Salif Mehmed <mail@salif.eu>
2021-02-10 15:04:42 +0200

    update

--- home/salif/.config/fish/functions/fish_prompt.fish
+++ home/salif/.config/fish/functions/fish_prompt.fish
@@ -1,33 +1,67 @@
 function fish_prompt
-	set -l last_status $status
-    if not set -q VIRTUAL_ENV_DISABLE_PROMPT
-        set -g VIRTUAL_ENV_DISABLE_PROMPT true
-    end
-    set_color $fish_color_user
-    printf '%s' $USER
-    set_color normal
-    printf ' at '
 
-    set_color magenta
+    set -l retc red
+    test $status = 0; and set retc green
+
+    set -q __fish_git_prompt_showupstream
+    or set -g __fish_git_prompt_showupstream auto
+
+    function _nim_prompt_wrapper
+        set retc $argv[1]
+        set field_name $argv[2]
+        set field_value $argv[3]
+
+        set_color normal
+        set_color $retc
+        echo -n '─'
+        set_color -o green
+        echo -n '['
+        set_color normal
+        test -n $field_name
+        and echo -n $field_name:
+        set_color $retc
+        echo -n $field_value
+        set_color -o green
+        echo -n ']'
+    end
+
+    set_color $retc
+    echo -n '┬─'
+    set_color -o green
+    echo -n [
+    if test "$USER" = root -o "$USER" = toor
+        set_color -o red
+    else
+        set_color -o blue
+    end
+    echo -n $USER
+    set_color -o white
+    echo -n @
+    set_color -o blue
     echo -n (prompt_hostname)
-    set_color normal
-    printf ' in '
+    set_color -o white
+    echo -n :(prompt_pwd)
+    set_color -o green
+    echo -n ']'
 
-    set_color $fish_color_cwd
-    printf '%s' (prompt_pwd)
-    set_color normal
+    # Date
+    _nim_prompt_wrapper $retc '' (date +%X)
 
-    # Line 2
+    # New line
     echo
-    if test $VIRTUAL_ENV
-        printf "(%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
-    end
-    
-    if not test $last_status -eq 0
-        set_color $fish_color_error
-    end
 
-    printf '➤ '
-    
+    # Background jobs
+    set_color normal
+    for job in (jobs)
+        set_color $retc
+        echo -n '│ '
+        set_color brown
+        echo $job
+    end
+    set_color normal
+    set_color $retc
+    echo -n '╰─>'
+    set_color -o red
+    echo -n '$ '
     set_color normal
 end
